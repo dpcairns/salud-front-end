304c123e5a746ad3abd45b4ad28993d6e2e11f7b
Rex Bergeron <rexbergeron@comcast.net>
2020-03-02 15:39:55 -0800

    suri - created front end repo

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React,  { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from 'react-router-dom';
+// import './App.css';
+// import Header from './Header.js';
+// import Search from './Search';
+// import Favorites from './Favorites.js';
+// import Login from './Login';
+// import PrivateRoute from './PrivateRoute';
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          
+        </Router>
+      </div>
+    );
+  }
+}
