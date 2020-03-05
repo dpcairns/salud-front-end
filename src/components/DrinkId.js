8ddb2793384c95b7b01803d5c29d90a72a07423c
surelis segarra <sdarianne34@gmail.com>
2020-03-04 17:49:28 -0800

    added a css file

--- src/components/DrinkId.js
+++ src/components/DrinkId.js
@@ -1,57 +1,64 @@
 
 import React, { Component } from 'react'
-
+import '../drinkID.css';
 export default class DrinkId extends Component {
     render() {
         console.log('hello world')
         const { cocktail } = this.props;
         console.log('|| props', this.props);
         return (
-            <div className='drink-name'>
-
+          <>
                 <h2>
                     {cocktail.Name}
                 </h2>
-                <img src ={cocktail.Image} alt=""/>
-                <p>
+            <div className='drink-name'>
+
+                <img className='detail-image' src ={cocktail.Image} alt=""/>
+
+                <div>
+                <p className= 'drink-id'>
                     {cocktail.id}
                 </p>
         
-                    <ul>
-                 <li>
-                  {cocktail.Ingredients[0]}" "
+                      <p className='ing-ptag'>Ingredients: </p>
+
+                 <ul className='ing'>
+                 <li className='ing-li'> 
+                  {cocktail.Ingredients[0]}
                   {cocktail.Measurement[0]}
                 </li>  
                 <li>
-                  {cocktail.Ingredients[1]}" "
+                  {cocktail.Ingredients[1]}
                   {cocktail.Measurement[1]}
                 </li> 
                 <li>
-                  {cocktail.Ingredients[2]}" "
+                  {cocktail.Ingredients[2]}
                   {cocktail.Measurement[2]}
                 </li> 
                 <li>
-                  {cocktail.Ingredients[3]}" "
+                  {cocktail.Ingredients[3]}
                   {cocktail.Measurement[3]}
                 </li> 
                 <li>
-                  {cocktail.Ingredients[4]}" "
+                  {cocktail.Ingredients[4]}
                   {cocktail.Measurement[4]}
                 </li> 
                 <li>
-                  {cocktail.Ingredients[5]}" "
+                  {cocktail.Ingredients[5]}
                   {cocktail.Measurement[5]}
                 </li> 
                 <li>
-                  {cocktail.Ingredients[6]}" "
+                  {cocktail.Ingredients[6]}
                   {cocktail.Measurement[6]}
                 </li> 
                     </ul>
-                <p>
+                    <p className='instructions-ptag'>Instructions:</p>
+                <p className='instructions'>
                 {cocktail.Instructions}
                 </p>
-
             </div>
+            </div>
+            </>
         )
     }
 }
