393b99803a148034c9edf7fe4d0f9e832bf3132f
Rex Bergeron <rexbergeron@comcast.net>
2020-03-04 17:42:22 -0800

    favorites page is being populated -rex and dorje over waaaaaaay too many hours

--- src/components/Random.js
+++ src/components/Random.js
@@ -21,8 +21,10 @@ export default class RandomList extends Component {
         const user = JSON.parse(localStorage.getItem('user'))
         const getRandomList = () => request.get(`https://mighty-plateau-34350.herokuapp.com/random`)
             .set('Authorization', user.token);
+
         const getFavorites = () => request.get('https://mighty-plateau-34350.herokuapp.com/favorites')
             .set('Authorization', user.token); 
+
         const fave = await getFavorites()
         const data = await getRandomList()
         console.log(data.body)
@@ -32,27 +34,27 @@ export default class RandomList extends Component {
     })
 }
 makeFavorite = async (drink) => {
-    console.log('List', this.props)
+    console.log('this works', drink)
     // when the user clicks the makeFavorite button, add this drink to the favorite list
     const user = JSON.parse(localStorage.getItem('user'))
     const fave = await request.post('https://mighty-plateau-34350.herokuapp.com/favorites', {
         name: drink.name,
-        image: drink.image,
+        image: drink.image
        
     })
     .set('Authorization', user.token)
     console.log('fave', fave.body)
 }
-renderButtonOrStar = (drink) => {
+renderButton = (drink) => {
     // check the favorites list if we're on the search page
-    const isOnFavoritesList = this.state.favorites.find(person => drink.name === person.name);
+    const isOnFavoritesList = this.state.favorites.find(cocktails => drink.name === cocktails.name);
     if (!isOnFavoritesList) {
-        // if they're not on the list, give user option to add them to favorites
-        // we are iterarting through a list, and we need the item in a function, so we make an anonymous function that CALLS that function on click with the right arguments
-    return <button onClick={ (e) => this.makeFavorite(drink)}>Make favorite</button>
+    
+    return <button onClick={ () => this.makeFavorite(drink)}>Make favorite</button>
     }
     // otherwise, indicate that they ae already on the favorites list
-    return <span>⭐</span>
+    // this.makeFavorite(drink.id)
+    return <span>🍸</span>
 }
 render() {
     return (
@@ -61,11 +63,13 @@ render() {
                 <ul className='random-list'>
             {
                 this.state.cocktail.map(cocktail => (
+                    <>
                     <div key={cocktail.id} to={`/id/${cocktail.id}`} onClick={()=> this.props.history.push(`/id/${cocktail.id}`)}> 
 
                     <CocktailItem cocktail={cocktail}/>
-                    {this.renderButtonOrStar(cocktail)}
                     </div>
+                    {this.renderButton(cocktail)}
+                    </>
                 )
                 )   
                 }
--- src/components/favoriteslist.js
+++ src/components/favoriteslist.js
@@ -2,31 +2,33 @@ import React, { Component } from 'react';
 import request from 'superagent';
 import { withRouter } from 'react-router-dom';
 
+
+
 export default withRouter(class favoritesList extends Component {
-    makeFavorite = async(drink) => {
-        const fave = await request.post('https://mighty-plateau-34350.herokuapp.com/favorites', {
-            name: drink.name,
-            image: drink.image,
-
-        })
-        .set('Authorization', this.props.user.token)
+    state = {
+        favorites:[]
     }
 
-    renderButton = (drink) => {
-        const onFavoritesList = this.props.favorites.find(favorites => drink.name === favorites.name);
-        if (!onFavoritesList) {
-            return <button onClick={(e)=> this.makeFavorite(drink)}Favorite>I like this Drink</button>
-        }
-        return <span>🍸</span>
-    }
+    async componentDidMount() {
+
+        const user = JSON.parse(localStorage.getItem('user'))
+        const getFavoritesList = () => request.get('https://mighty-plateau-34350.herokuapp.com/favorites')
+            .set('Authorization', user.token);
+            
+    const fave = await getFavoritesList()
+    console.log(fave.body)
+    this.setState({
+        favorites: fave.body
+    })
+}
 
     render() {
         return(
             <div>
                 {
-                    this.props.favorites.map(drink => <div key={drink.name} className="drink-box">
+                    this.state.favorites.map(drink => <div key={drink.name} className="drink-box">
                         <div>{drink.name}</div>
-                        <div>{drink.image}</div>
+                        <img src={drink.image} alt=""/>
                         {
                             this.props.location.pathname !== '/favorites'
                             && this.renderButton(drink) 
