70fddd7c774a7cff5318f4af5c28ee199dfeabdb
Rex Bergeron <rexbergeron@comcast.net>
2020-03-06 12:14:39 -0800

    dorje null function

--- src/components/DrinkId.js
+++ src/components/DrinkId.js
@@ -2,60 +2,31 @@ import React, { Component } from 'react'
 import '../drinkID.css';
 export default class DrinkId extends Component {
     render() {
-       
         const { cocktail } = this.props;
-       
-        return (
-          
+        const makeDrinkStrings = (ingredientArray, measurementArray) => {
+            return ingredientArray.map((ingredient, i) => {
+              if (ingredient !== null ) {
+                return ingredient + ' ' + measurementArray[i]
+              }
+            })
+          }
+        const stringArray = makeDrinkStrings(cocktail.Ingredients, cocktail.Measurement) 
+        const elementArray = stringArray.map(string => (
+          <li>{string}</li>
+        ))
+          return (
             <div className='drink-name'>
                   <h2>{cocktail.Name}</h2>
-
                   <img className='detail-image' src ={cocktail.Image} alt=""/>
-
-               
-
-        
                          <p className='ing-ptag'>Ingredients: </p>
-          
                  <ul className='ing'>
-                      <li className='ing-li'> 
-
-                        {cocktail.Ingredients[0]} 
-                        {cocktail.Measurement[0]}
-                      </li>  
-                      <li>
-                        {cocktail.Ingredients[1]} 
-                        {cocktail.Measurement[1]}
-                      </li> 
-                      <li>
-                        {cocktail.Ingredients[2]}
-                        {cocktail.Measurement[2]}
-                      </li> 
-                      <li>
-                        {cocktail.Ingredients[3]}
-                        {cocktail.Measurement[3]}
-                      </li> 
-                      <li>
-                        {cocktail.Ingredients[4]}
-                        {cocktail.Measurement[4]}
-                      </li> 
-                      <li>
-                        {cocktail.Ingredients[5]}
-                        {cocktail.Measurement[5]}
-                      </li> 
-                      <li>
-                        {cocktail.Ingredients[6]} 
-                        {cocktail.Measurement[6]}
-                      </li> 
+                     {elementArray}
                     </ul>
                     <p className='instructions-ptag'>How to make your tasty drink:</p>
                     <p className='instructions'>
                       {cocktail.Instructions}
                      </p>
-            
-             
           </div>
-          
         )
     }
 }
--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import request from 'superagent';
-//import { Link } from 'react-router-dom';
 import CocktailItem from './CocktailItem';
 
 
