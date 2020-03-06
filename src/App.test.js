19e89ce88efdb22f3684da32dd5fbef03ac908f1
dorjepadma <dorjepadma@gmail.com>
2020-03-06 10:03:08 -0800

    fixed ingredients and measurements dorje

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import React from 'react';
+// import App from './App';
+import renderer from 'react-test-renderer';
+// import VodkaList from './components/VodkaList.js';
+// import Popular from './components/Popular.js';
+import TequilaList from './components/TequilaList';
+
+// test('renders VodkaList correctly', () => {
+//   const vodka = renderer
+//     .create(<VodkaList />)
+//     .toJSON();
+//   expect(vodka).toMatchSnapshot();
+// });
+
+// test('renders popular list correctly', () => {
+//   const popular = renderer
+//     .create(<Popular />)
+//     .toJSON();
+//   expect(popular).toMatchSnapshot();
+// });
+
+test('renders tequila list correctly', () => {
+  const tequila = renderer
+    .create(<TequilaList />)
+    .toJSON();
+  expect(tequila).toMatchSnapshot();
 });
+
+
